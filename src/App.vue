375269c00a6e0c40eb6247c4e1fbbf2cbc077f92
Mandeep Tariyal <tariyalmandeep@gmail.com>
2020-10-13 13:42:08 +0530

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title />
+    <ul>
+      <ListItem v-for="item in items" :item="item" :key="item.id"/>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ListItem from './components/ListItem.vue';
+import Title from './components/Title.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    ListItem,
+    Title
+  },
+  data(){
+    return {
+      newMsg: "Welcome to Your Vue.js + TypeScript App",
+      items: [
+        {id: 1, name: "buy tea"},
+        {id: 2, name: "buy banana"}
+        ]
+    };
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>{{ item.name }}</li>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Title from './Title.vue';
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    item: Object,
+  }
+});
+</script>
